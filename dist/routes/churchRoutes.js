3c697056b8eca56675b4a321ecdc3bac6b3deb79
lwooden <lowell.wooden@gmail.com>
2025-06-26 08:44:31 -0400

    removing dist

--- dist/routes/churchRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const churchController_1 = require("../controllers/churchController");
-const router = (0, express_1.Router)();
-router.get("/", churchController_1.getChurches);
-// router.get("/:id", getChurchById)
-router.post("/", churchController_1.createChurch);
-exports.default = router;
