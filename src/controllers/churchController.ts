5bfb682c1e27d113bffd74cc68d8954a7cb39fa6
lwooden <lowell.wooden@gmail.com>
2025-06-28 16:30:29 -0400

    clean-up; remove drizzle stuff

--- src/controllers/churchController.ts
+++ src/controllers/churchController.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from "express"
-//import { drizzle } from "drizzle-orm/node-postgres"
 import { connectToDatabase } from "../database/mongoose"
 import Church from "../database/church.model"
-
 
 export const createChurch = async (
   req: Request,
@@ -12,12 +10,13 @@ export const createChurch = async (
   try {
     await connectToDatabase()
 
-    const { churchId, name, address, city, state, zipcode } = req.body
+    const { name, address, city, state, zipcode, description, picture } =
+      req.body
 
-    const newChurch = Church.create({
-      churchId,
+    const newChurch = await Church.create({
       name,
       address,
+      description,
       city,
       state,
       zipcode,
@@ -28,34 +27,6 @@ export const createChurch = async (
     next(error)
   }
 }
-
-// //create controller for getting all churches from mongoose database
-// export const getChurch = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   try {
-//     await connectToDatabase()
-
-//     const churchId = req.params.id
-//     if (!churchId) {
-//       return res.status(400).json({ message: "Church ID is required" })
-//     }
-
-//     const church = await Church.findById(churchId)  // Assuming Church is a Mongoose model
-//     if (!church) {
-
-//       return res.status(404).json({ message: "Church not found" })
-//     } else {
-//       console.log("Getting church from the database: ", church)
-//       res.status(200).json(church)
-//     }
-//   } catch (error) {
-//     next(error)
-//   }
-
-// }
 
 export const getAllChurches = async (
   req: Request,
@@ -71,4 +42,4 @@ export const getAllChurches = async (
   } catch (error) {
     next(error)
   }
-} 
+}
