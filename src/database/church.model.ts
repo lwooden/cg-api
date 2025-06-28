8cdab00d755ed66a3f60aa3d1d593231142ccc40
lwooden <lowell.wooden@gmail.com>
2025-06-28 16:31:51 -0400

    schema Types update for proper ObjectID mapping

--- src/database/church.model.ts
+++ src/database/church.model.ts
@@ -1,55 +1,55 @@
-import { model, models, Document, Schema } from "mongoose"
+import { model, models, Document, Schema, Types } from "mongoose"
 
 export interface IChurch extends Document {
-    churchId: string
-    name: string
-    address: string
-    city: string
-    state: string
-    zipcode: string
-    description?: string
-    picture?: string
-    createdAt: Date
+  churchId: Schema.Types.ObjectId
+  name: string
+  address: string
+  city: string
+  state: string
+  zipcode: string
+  description?: string
+  picture?: string
+  createdAt: Date
 }
 
 const ChurchSchema = new Schema({
-    churchId: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    zipcode: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        default: "",
-    },
-    picture: {
-        type: String,
-        default: "",
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
+  churchId: {
+    type: Schema.Types.ObjectId,
+    default: () => new Types.ObjectId(),
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zipcode: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  picture: {
+    type: String,
+    default: "",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 })
 
 const Church = models.Church || model<IChurch>("Church", ChurchSchema)
